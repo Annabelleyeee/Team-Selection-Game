54cf3124cbceb46f6b74cfca441750923a156ed7
Annabelle Yee <78177094+Annabelleyeee@users.noreply.github.com>
2021-06-24 21:42:06 +0000

    Added commments and spacing

--- index.js
+++ index.js
@@ -2,44 +2,49 @@
 //Annabelle Yee
 //23/06/2021
 
-//having a list of players ready to be picked 
-let players = ['Anna', 'Abi', 'Annabelle', 'Zoe', 'Xanthe', 'Georgia', 'Lily', 'Ella', 'Samantha', 'Bob', 'Sal', 'Jo'];
+
+ console.log ('Welcome to this virtual random selection tool! \n\nFrom a list of names, this tool will randomly file the names into 2 teams')
+
+// this is the list of players ready to be picked from 
+let players = [' Anna', ' Abi' , ' Annabelle', ' Zoe', ' Xanthe', ' Georgia', ' Lily', ' Ella', ' Samantha', ' Bob', ' Sal', ' Jo'];
+
+console.log ('\nThis programme will be sorting the following people into 2 teams: \n[' + players + '] \n')
 
 //having two empty teams waiting to have players to be added
 let team1 = [];
 let team2 = [];
+//setting the round number to 1 and length to the number of players in our 'players' list
 let round = 1;
 let length = players.length;
 
 do {
+
   //pick a random member from 'players' list
   let player1 = Math.floor(Math.random() * length + 0);
-  // console.log(player1);
   let player1Name = players[player1];
-  // console.log(player1Name);
-
-  //to remove the random person picked from the 'players' list 
+  
+ //to remove the random person picked from the 'players' list 
   players.splice(player1, 1);
-  // console.log(players);
+ 
 
   //to add the random person picked to Team1
   console.log('Team 1: ' + player1Name);
   team1.push (player1Name)
+  //adding a round and telling the programme that it has one less name to pick from.
   round++;
   length--;
 
   //give Team2 a member 
   //pick a random member from 'players' list
   let player2 = Math.floor(Math.random() * length + 0);
-  // console.log(player2);
+  
   let player2Name = players[player2];
-  // console.log(player2Name);
 
   //to remove the random person picked from the 'players' list 
   players.splice(player2, 1);
-  // console.log(players);
+  
 
-  //to add the random person picked to Team1
+  //to add the random person picked to Team2
   console.log('Team 2: ' + player2Name);
   team2.push(player2Name)
 
@@ -47,6 +52,6 @@ do {
   length--;
 
 } while (round <= 12);
-console.log("Team 1 is: " + team1);
-console.log("Team 2 is: " + team2);
+console.log('\nTeam 1 is: ' + team1);
+console.log('Team 2 is: ' + team2);
 
